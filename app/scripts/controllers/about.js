f29394ff6eaa7542eae83decb32fb9c5414ed4eb
Lucas Andrey <andreybleme1@gmail.com>
2015-12-05 23:42:37 -0200

    Angular initial stricture

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name oddyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the oddyApp
+ */
+angular.module('oddyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name oddyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the oddyApp
+ */
+angular.module('oddyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
